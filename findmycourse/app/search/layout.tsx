44f35d9b14930ffbf3d3e5e388cc3e6583544d19
saymanq <87442765+saymanq@users.noreply.github.com>
2025-02-17 10:50:46 -0700

    Final Version

--- findmycourse/app/search/layout.tsx
+++ findmycourse/app/search/layout.tsx
@@ -1,14 +1,16 @@
-export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <div className="min-h-screen bg-[hsl(240,10%,3.9%)] py-2 px-1 sm:px-3 lg:px-4 relative">
-        {/* Background dots */}
-        <div className="absolute inset-0">
-          <div
-            className="absolute h-full w-full bg-[radial-gradient(white_1px,transparent_1px)] opacity-20"
-            style={{ backgroundSize: '20px 20px' }}
-          />
-        </div>
-        <div>{children}</div>
+
+
+
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="min-h-screen bg-[hsl(240,10%,3.9%)] py-2 px-1 sm:px-3 lg:px-4 relative">
+      <div className="absolute inset-0">
+        <div
+          className="absolute h-full w-full bg-[radial-gradient(white_1px,transparent_1px)] opacity-20"
+          style={{ backgroundSize: '20px 20px' }}
+        />
       </div>
-    )
+      <div>{children}</div>
+    </div>
+  )
 }
